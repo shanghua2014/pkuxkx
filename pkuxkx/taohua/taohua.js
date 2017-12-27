52a1f97f4ad75e5b3ea1a95eefa327ccb121d22c
shanghua.zhao <shanghua.zhao@huanxi.fm>
2017-12-27 21:23:25 +0800

    添加：桃花新手过阵逻辑

--- pkuxkx/taohua/taohua.js
+++ pkuxkx/taohua/taohua.js
@@ -1,13 +1,72 @@
 
 
 
+/*---------------- [新手任务] -----------------*/
+var job = {
+	guawei : '',
+	position:[],
+	i:0,
+	lookTu: function ( guawei ) {
+		this.guawei = guawei;
+		EnableTriggerGroup('guozhen', 1);
+		DoAfterSpecial(1, 'look tu', 10);
+	},
+	zhenCN: function (guawei, p1, p2, p3, p4) {
+		this.position = [];
+		if (guawei == this.guawei){
+			this.position.push(reversePathCN(p1));
+			this.position.push(reversePathCN(p2));
+			this.position.push(reversePathCN(p3));
+			this.position.push(reversePathCN(p4));
+			SetVariable('position', this.position);
+			SetStatus('位置：' + p1 + '------' + p2 + '------' + p3 + '------' + p4 + ':::' + this.position);
+			DoAfterSpecial(1, '4e', 11);
+		}
+	},
+	zouwei : function ( reset ) {
+		if (reset == 0) {	// 走错位，重新走
+			this.i = 0;
+			send('hpbrief');
+		}
+		if (this.i > 3) this.i = 0;
+		this.position = GetVariable('position').split(',');
+		note('走了一步：'+this.position[this.i]);
+		DoAfterSpecial(2, this.position[this.i], 10);
+		this.i++;
+	},
+	busy : function () {
+		this.position = GetVariable('position').split(',');
+		if (this.i != 0) --this.i;
+		DoAfterSpecial(2, this.position[this.i], 10);
+		this.i++;
+	},
+	back : function () {
+		note('到点出阵了！');
+		send('leave');
+		send('jiali 0');
+		DoAfterSpecial(1, '3w', 11);
+		DoAfterSpecial(3, '(ask lu about job)', 11);
+	},
+	kill : function ( npc ) {
+		if (npc) {
+			send('jiali max');
+			send('l');
+			SetVariable('zhen_npc_ch', npc);
+		} else {
+			var npc = GetVariable('zhen_npc_en');
+			note(npc);
+			DoAfterSpecial(2, "(kill "+npc+')', 11);
+		}
+	}
+}
+
 /*---------------- [睡觉] -----------------*/
 var base = {
 	sleep:function (i, cmd) {
 		// 醒来
 		if (i == 0) { DoAfterSpecial(.5, cmd, 10); }
 		// 睡觉
-		if (i == 1) { send(cmd); }
+		if (i == 1) { send(cmd); send('hpbrief'); }
 	},
 	sleepDelay:function (time, cmd) {
 		note('睡多了，等会吧。');
@@ -15,6 +74,14 @@ var base = {
 	}
 }
 note('====== 所有触发已关闭，请启动相关任务！');
+EnableTriggerGroup('dazuo', 0)
+EnableTriggerGroup('tuna', 0)
+EnableTriggerGroup('dushu', 0)
+EnableTriggerGroup('lian', 0)
+EnableTriggerGroup('guozhen', 0)
+EnableTriggerGroup('ludu', 0)
+EnableTriggerGroup('snow', 0)
+EnableTriggerGroup('xuexi', 0)
 
 
 //--------------------------------------------
@@ -38,6 +105,36 @@ function timer (t) {
 	SetTimerOption(t.name, "send_to", t.where);
 	SetTimerOption(t.name, "send", t.cmd);
 	SetTimerOption(t.name, "one_shot", t.once);
+}
+function reversePathCN(path) {
+	var p = '';
+	switch (path) {
+		case '东':
+			p='e';
+			break;
+		case '西':
+			p='w';
+			break;
+		case '南':
+			p='s';
+			break;
+		case '北':
+			p='n';
+			break;
+		case '东北':
+			p='ne';
+			break;
+		case '西北':
+			p='nw';
+			break;
+		case '东南':
+			p='se';
+			break;
+		case '西南':
+			p='sw';
+			break;
+	}
+	return p;
 }
 /**
  * 
